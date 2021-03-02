c4559fbd1e366255e266afe41f1e192de6d224a9
suraj813 <suraj813@gmail.com>
2021-03-02 16:31:30 -0500

    add playlist wip

--- flask/app.py
+++ flask/app.py
@@ -18,19 +18,22 @@ MAX_AUDIO_DURATION = 6500
 
 
 class S3Helper:
-    def __init__(self, folder):
-        self.bucket = 'demucs-app-cache'
+    def __init__(self, folder=None):
+        self.BUCKET = 'demucs-app-cache'
         self.folder = folder
-        bucket_url = f'http://{self.bucket}.s3.amazonaws.com/'
-        self.access_point = bucket_url + folder
 
-    # ls grep s3
-    def _file_in_cache(self, object_name):
+    # ls
+    def _ls(self):
+        s3_client = boto3.client('s3')
+        contents = s3_client.list_objects_v2(Bucket=self.BUCKET, Delimiter='/vocals.mp3')
+        return contents
+
+
+    # grep s3
+    def _grep(self, object_name):
         s3_client = boto3.client('s3')
         try:
-            r = s3_client.head_object(
-                Bucket=self.bucket,
-                Key=object_name)
+            r = s3_client.head_object(Bucket=self.BUCKET, Key=object_name)
         except ClientError:
             logger.debug(f"{object_name} doesn't exist in S3 cache")
             return False
@@ -42,20 +45,31 @@ class S3Helper:
         logger.debug(f'Uploading {object_name}')
         s3_client.upload_fileobj(
             io.BytesIO(bytes_like),
-            self.bucket,
+            self.BUCKET,
             object_name,
             ExtraArgs={'ACL':'public-read'})
         return True
 
     def cache_available(self):
-        return self._file_in_cache(self.folder + '/' + 'vocals.mp3')
+        return self._grep(self.folder + '/' + 'vocals.mp3')
+
+    def get_filelist(self):
+        contents = self._ls()['CommonPrefixes']
+        ids = [d['Prefix'].split('/vocals.mp3')[0] for d in contents]
+        return ids
+    
+    def get_access_point(self):
+        if self.folder:
+            return f'http://{self.BUCKET}.s3.amazonaws.com/{self.folder}'
 
     @logger.catch
     def upload_stems(self, stem_bytes):
+        if not self.folder:
+            raise ValueError('video id not initialized')
         for name, byt in stem_bytes.items():
             obj_name = self.folder + '/' + name + '.mp3'
             self._upload_file(byt, obj_name)
-        return True
+
 
    
 # get ETA + other things
@@ -71,7 +85,7 @@ def get_video_info(url):
     return response
 
 
-# get the youtube audio in bytes
+# get the youtube info_dict
 def youtubedl(url, download=True):
     logger.debug(f"Running youtube-dl for {url}")
     temp = tempfile.TemporaryDirectory()
@@ -84,12 +98,13 @@ def youtubedl(url, download=True):
             'preferredcodec': 'mp3',
             'preferredquality': '192',}]
         }
-    mp3_bytes = None
+
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(url, download=download)
         if download:
             out_file = Path(temp.name) / info_dict['id'] / 'original.mp3'
             info_dict['mp3_bytes'] = open(out_file, 'rb').read()
+    
     return info_dict
 
 
@@ -138,6 +153,14 @@ def validate_url(url):
     return True, (200, 'OK')
 
 
+@app.route("/api/playlist")
+@cross_origin()
+def get_cached():
+    ids = S3Helper().get_filelist()
+    
+
+
+
 @app.route("/api/info")
 @cross_origin()
 def info():
@@ -162,10 +185,9 @@ def main():
     info_dict = youtubedl(url)
     s3 = S3Helper(info_dict['id'])
     logger.info(f"Initialized S3 helper for {info_dict['id']}. Checking cache...")
+
     if not s3.cache_available():
-        logger.info("Not in cache. Running inference...")
         stem_bytes = run_inference(info_dict['mp3_bytes'])
-        logger.info("Inference done. Caching results...")
         upload_success = s3.upload_stems(stem_bytes)
         if upload_success:
             logger.info(f"Upload to cache success!")
@@ -173,10 +195,8 @@ def main():
             logger.error("Upload to cache failed!")
             return response
 
-    logger.info(f'Returning stems at {s3.access_point}')
     response['status'] = 200
-    response['msg'] = s3.access_point
-
+    response['msg'] = s3.get_access_point()
     return response
 
 
